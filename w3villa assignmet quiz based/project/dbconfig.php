a2d3987c6956af762b499cefad6c997598cae191
saurabh-rathaur <82214788+saurabh-rathaur@users.noreply.github.com>
2021-04-10 12:36:53 +0530

    Delete w3villa assignmet quiz based directory

--- w3villa assignmet quiz based/project/dbconfig.php
+++
@@ -1,9 +0,0 @@
-<?php
-$con = mysqli_connect("localhost","root","","database_of_connection");
-
-// Check connection
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
-?>
