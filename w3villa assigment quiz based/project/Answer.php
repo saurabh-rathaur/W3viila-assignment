ac2c7207a159bed361c41a36ca63208d975e44e9
saurabh-rathaur <82214788+saurabh-rathaur@users.noreply.github.com>
2021-04-10 13:56:02 +0530

    Add files via upload

---
+++ w3villa assigment quiz based/project/Answer.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body  style="background-color:pink;">
+<center>
+<h2> Answer of the Easy level quiz</h2> </center></center>
+<h2> 1 => int  </h2>
+<h2> 2 =>unsigned short </h2>
+<h2> 3 =>Depends on the system/compiler </h2>
+<h2> 4 =>PHP</h2>
+<h2> 5 =>Short is the qualifier and int is the basic data type  </h2>
+
+
+
+
+
+
+
+</body>
+</html> 
