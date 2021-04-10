ac8fa14b342641a12f2c86146c0a89d8c849a216
saurabh-rathaur <82214788+saurabh-rathaur@users.noreply.github.com>
2021-04-10 12:35:06 +0530

    Add files via upload

---
+++ w3villa assigment quiz based/project/Answer2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body  style="background-color:pink;">
+<center>
+<h2> Answer of the Hard level quiz</h2> </center></center>
+<h2> 1 => Bytecode is executed by JVM  </h2>
+<h2> 2 => Use of pointers </h2>
+<h2> 3 => Unicode escape sequence  </h2>
+<h2> 4 =>JDB</h2>
+<h2> 5 => Infinity </h2>
+</body>
+</html> 
